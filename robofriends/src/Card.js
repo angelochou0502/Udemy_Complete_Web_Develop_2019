bb9d7a284ce41dd50b9d0ec8b780ed702201a9d2
angelo0502 <b03902016@ntu.edu.tw>
2019-08-02 23:45:47 +0800

    writing robofriends app

---
+++ robofriends/src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'tachyons'
+
+
+function Card({id, name, email}) {
+	return (
+		<div className="tc bg-light-pink dib br3 pa3 ma3 grow shadow-5">
+			<img alt="BUG!" src={`https://robohash.org/${id}`}/>
+			<div>
+				<h2>{name}</h2>
+				<p>{email}</p>
+			</div>
+		</div>
+		)
+
+};
+
+export default Card;
